797bc51c760f8ae9101c2b489b1598c3c95d1b44
uid <uid@dhcp-18-111-33-29.dyn.mit.edu>
2014-11-17 14:14:30 -0500

    button comments

--- IOS8SwiftPrototypeCellsTutorial/ViewController.swift
+++ IOS8SwiftPrototypeCellsTutorial/ViewController.swift
@@ -77,11 +77,81 @@ var favoritePhrases = ["Hello I'm saved"]
 
 @IBAction func sendInputText(sender: UIButton) {
 let text = textToPlayField.text
-historyPhrases.append(text)
-tableView.reloadData()
-playText(text)
-showInputTextAlert(text)
+if (text != "") { //_____________ADDED CHANGEEEEEEEEE _________
+    
+    historyPhrases.append(text)
+    tableView.reloadData()
+    playText(text)
+    showInputTextScreen(text)
+    }
 }
+    //http://prateekvjoshi.com/2014/02/16/ios-app-passing-data-between-view-controllers/
+    FIRST CONTROLLER 
+    import secondController
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject) {
+        var secondViewController: SecondViewController = segue.destinationViewController as SecondViewController
+        var text = textToPlayField.text
+        secondViewController.text = text
+    }
+    http://ios-blog.co.uk/tutorials/developing-ios8-apps-using-swift-part-6-interaction-with-multiple-views/
+    
+    
+    //make a new screen  on page  -->new viewcontroller (Option-click on a button on one screen and drag over to the other screen and release to create a segue.
+    //make a close button on this second screen
+    //http://mathewsanders.com/animated-transitions-in-swift/
+    //http://mathewsanders.com/custom-menu-transitions-in-swift/#fn:projects
+    //control click on button and drag and release to screen exit segue icon
+
+    //method  for the exit segue of this screen:  removes current screen that is on top (so doesn't matter whats below yayy)
+    @IBAction func unwindToViewController (sender: UIStoryboardSegue) {
+    } //may go in second controller....
+    
+    
+    //SECOND VIEW CONTROLLER
+    import UIKit
+    need property String text
+    @IBOutlet var textLabel: UILabel! ???
+    
+    class MenuViewController: UIViewController {
+    
+    // create instance of our custom transition manager
+    let transitionManager = MenuTransitionManager()
+    
+    override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+    self.transitioningDelegate = self.transitionManager
+    
+    }
+    
+    override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+    }
+    
+    
+    }
+    
+    add play button and favorite button!!!!!! (look up code to add buttons)
+    add button using object library http://www.raywenderlich.com/77176/learn-to-code-ios-apps-with-swift-tutorial-4
+    for play and favorite, close
+    add constraints to container, add title
+    @IBAction func playButtonPressed() {
+        NSLog("PlayButtonPressed")
+    }
+    
+    @IBAction func favoriteButtonPressed() {
+        NSLog("favoriteButtonPressed)
+    }
+    
+    @IBAction func closeButtonPressed() {
+        NSLog("closeButtonPressed)
+    }
+    
+    
+    
+    ..may need transition manager class for basic fade out transition
+
 
 //shows the popup screen featuring the recently input text, that the user can replay or save
 func showInputTextAlert(text:String) {
